f90ebc6e824354fe7d1ec87204235329ad02ec4f
Ivan_Sumilin <shumilin.i.v@gmail.com>
2021-08-16 22:16:20 +0300

    addition .gitignore

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+from brain_games.scripts.cli import welcom_user
 
 def main():
     print('Welcome to the Brain Games!')
+    welcom_user()
     return
 
 if __name__ == "__main__":
